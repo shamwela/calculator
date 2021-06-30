ebe101b502cb2724347dde73b503a3a939b6a7d5
Sha Mwe La <shamwela2002@gmail.com>
2021-06-30 15:38:57 +0630

    Make the layout more detailed

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import './App.sass';
 function App() {
   return (
     <div id="app">
-      <form id="calculator">
+      <div id="calculator">
         <input type="text" id="display" />
-        <div>
+
+        <button>AC</button>
+        <button>+/-</button>
+        <button>%</button>
+        <button className="operator">÷</button>
+
+        <div id="number-area">
           {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
             <button key={number} className={number}>
               {number}
             </button>
           ))}
         </div>
-      </form>
+
+        <button className="operator">x</button>
+        <button className="operator">-</button>
+        <button className="operator">+</button>
+        <button id="zero">0</button>
+        <button>.</button>
+        <button className="operator">=</button>
+      </div>
     </div>
   );
 }
