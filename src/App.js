519b5d945205fc2f078787376d9a5f7705d4ea71
Sha Mwe La <shamwela2002@gmail.com>
2021-06-30 14:36:20 +0630

    Create a rough layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.sass';
 
 function App() {
-  return <div id="app"></div>;
+  return (
+    <div id="app">
+      <form id="calculator">
+        <input type="text" id="display" />
+        <div>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
+            <button key={number} className={number}>
+              {number}
+            </button>
+          ))}
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
